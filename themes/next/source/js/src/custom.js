466ea181eefab926c3192652807eb35ed19bc475
Xingsheng Zhao <zhxshseu@gmail.com>
2018-03-29 15:05:20 -0500

    baidu_push

--- themes/next/source/js/src/custom.js
+++ themes/next/source/js/src/custom.js
@@ -113,4 +113,17 @@ $(document).ready(function() {
       $datecircle.find(".day").text(res[2]);
       $datecircle.find(".month").text(month[parseInt(res[1])]);
   });
+
+  // 百度推送
+  var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+
 });
