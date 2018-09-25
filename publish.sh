c030f83a1f55e5a767a15f99303d2d9bc53856cd
Xingsheng Zhao <zhxshseu@gmail.com>
2018-09-25 14:46:55 -0500

    new post

--- publish.sh
+++ publish.sh
@@ -1,10 +1,10 @@
 git add .
 git commit -m "new post"
 git push origin master
-export HEXO_ALGOLIA_INDEXING_KEY=aed9efacc66cdcd0647b6fe1e4808317
+# export HEXO_ALGOLIA_INDEXING_KEY=aed9efacc66cdcd0647b6fe1e4808317
 # hexo clean
 # gulp compressImage
 hexo g
-hexo algolia
+# hexo algolia
 gulp
 hexo d
